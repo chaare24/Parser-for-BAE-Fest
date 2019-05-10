c8ef56bf3ff3076e4f4b50088b99e04804880935
Charles Adrick Arellano <>
2019-05-10 15:21:40 -0700

    getListOfWords works
    Added test cases
    
    need to check list of words compared to list of strings

--- parser.py
+++ parser.py
@@ -1,16 +1,56 @@
 #! python3
-
+from os import listdir 
+from os.path import isfile, join
 import docx
 import sys
+import re
+from bs4 import BeautifulSoup
 
-x = str(sys.argv[1])
+# retrieve file names and stores it into a list
+files = [f for f in listdir(".") if (isfile(join( ".", f)) and f != "parser.py")]
+print(files)
 
-def getText(fileName):
+listOfWords = []
+
+def getListOfWords(fileName):
+    tempListOfWords = []
+    file = open(fileName, "r")
+    text = file.readlines()
+    tempListOfWords.extend(text)
+    
+    for word in tempListOfWords:
+        listOfWords.append(word.strip())
+    
+    print(listOfWords)
+
+# Able to retrieve text from the Word Documents only
+def getTextFromDocFiles(fileName):
     doc = docx.Document(fileName)
     fullText = []
+    changedText = []
     for paragraph in doc.paragraphs:
         fullText.append(paragraph.text)
+    for line in fullText: 
+        line.strip()
+        if line == '' or line.strip() == '':
+            continue
+        changedText.append(line.strip())
+
+    HTMLParser(changedText)
 
     return '\n'.join(fullText)
 
-print(getText(x))
+# HTML Validator
+# Should return the line number, line if false
+def HTMLParser(text):
+    for line in text:
+        if (line == '' or line == '\n'):
+            continue
+        if (bool (BeautifulSoup(line, "html.parser").find()) == False):
+            print(bool (BeautifulSoup(line, "html.parser").find()))
+            print(line)
+
+#getTextFromDocFiles(files[3])
+#getTextFromTextFiles(files[0])
+x = str(sys.argv[1])
+getListOfWords(x)
